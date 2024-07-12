59aa943eeb3c53d70957c3b0df21d737a0cf3cd2
Fran Lopez <franciscolopto@gmail.com>
2024-07-12 11:42:48 +0200

    wrongly formatted app.tsx going through pre-commit configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
   return (
     <>
-      <h1>Hola husky</h1>
+      <h1>Lista de Usuarios</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
-
 export default App;
